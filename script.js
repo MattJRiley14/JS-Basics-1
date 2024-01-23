63ff371ee908db1603ce1d7da4db9d0561aebda3
Matthew Riley <matthew.riley@barren.kyschool.us>
2024-01-23 10:50:51 -0600

    Complete Exercise

---
+++ script.js
@@ -0,0 +1,57 @@
+// JavaScript Basics Exercise
+// 1
+console.log(12);
+
+// 2
+// Number, Boolean, String, Null, & Undefined
+
+// 3
+// + for Addition, - for Subtraction, * for Multiplication, / for Division, ** for Exponents, % for Modulus
+
+// 4
+console.log(5 % 4); // 1
+// OR
+console.log(5 % 2); // 1
+
+// 5
+console.log('I\'M MARY POPPINS, Y\'ALL!'); // I'M MARY POPPINS, Y'ALL!
+
+// 6
+console.log("Hawk" + "eye"); // Hawkeye
+
+// 7
+console.log("Matt".length + "Riley".length); // 9
+
+// 8
+console.log("Avengers: Endgame"[16]); // e
+
+// BONUS
+// 1
+console.log("\\"); // \
+
+// 2
+console.log(5 ** 4 - 9); // 616
+// console.log(625 - 9);
+// console.log(616);
+
+// 3
+console.log("I".length - "Am".length + "Iron".length - "Man".length); // 0
+// console.log(1 - 2 + 4 - 3);
+// console.log(-1 + 1);
+// console.log(0);
+
+// 4
+console.log("CAP"[1]); // A
+console.log("VISION"[0]); // V
+console.log("OKOYE"[4]) // E
+console.log("NICK FURY"[0]); // N
+console.log("GROOT"[0]); // G
+console.log("DOCTOR STRANGE"[13]); // E
+console.log("THOR"[3]); // R
+console.log("STAR-LORD"[0]); // S
+
+// 5
+var m = 25;
+var c = 4;
+var u = m + c;
+console.log(u); // 29
